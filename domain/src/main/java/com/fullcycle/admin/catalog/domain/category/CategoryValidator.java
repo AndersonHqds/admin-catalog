da0bbfcc3f50fca43859f96006d6840ae92d4ea9
Anderson Silva <contatoahenrique@gmail.com>
2023-12-23 02:04:30 -0300

    Update exceptions and improve the Category Validation

--- domain/src/main/java/com/fullcycle/admin/catalog/domain/category/CategoryValidator.java
+++ domain/src/main/java/com/fullcycle/admin/catalog/domain/category/CategoryValidator.java
@@ -13,8 +13,22 @@ public class CategoryValidator extends Validator {
 
     @Override
     public void validate() {
-        if (this.category.getName() == null) {
+        checkNameConstraint();
+    }
+
+    private void checkNameConstraint() {
+        final var name = this.category.getName();
+        if (name == null) {
             this.validationHandler().append(new Error("'name' should not be null"));
+            return;
+        }
+        if(name.isBlank()) {
+            this.validationHandler().append(new Error("'name' should not be empty"));
+            return;
+        }
+        if(name.trim().length() > 255 || name.trim().length() < 3) {
+            this.validationHandler().append(new Error("'name' must be between 3 and 255 characters"));
+            return;
         }
     }
 }
--- domain/src/test/java/com/fullcycle/admin/catalog/domain/CategoryTest.java
+++ domain/src/test/java/com/fullcycle/admin/catalog/domain/CategoryTest.java
@@ -25,9 +25,23 @@ public class CategoryTest {
 
     @Test
     public void givenAnInvalidNullName_whenCallNewCategoryAndValidate_thenShouldReceiveError() {
-        final String expectedName = null;
         final var expectedErrorCount = 1;
         final var expectedErrorMessage = "'name' should not be null";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+
+        final var actualCategory = Category.newCategory(null, expectedDescription, expectedIsActive);
+
+        final var actualException = Assertions.assertThrows(DomainException.class, () -> actualCategory.validate(new ThrowsValidationHandler()));
+        Assertions.assertEquals(expectedErrorCount,actualException.getErrors().size());
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+    }
+
+    @Test
+    public void givenAnInvalidEmptyName_whenCallNewCategoryAndValidate_thenShouldReceiveError() {
+        final String expectedName = "  ";
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'name' should not be empty";
         final var expectedDescription = "A categoria mais assistida";
         final var expectedIsActive = true;
 
@@ -36,5 +50,76 @@ public class CategoryTest {
         final var actualException = Assertions.assertThrows(DomainException.class, () -> actualCategory.validate(new ThrowsValidationHandler()));
         Assertions.assertEquals(expectedErrorCount,actualException.getErrors().size());
         Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+    }
+
+    @Test
+    public void givenAnInvalidNameLengthLessThan3_whenCallNewCategoryAndValidate_thenShouldReceiveError() {
+        final String expectedName = "An ";
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'name' must be between 3 and 255 characters";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+
+        final var actualCategory = Category.newCategory(expectedName, expectedDescription, expectedIsActive);
+
+        final var actualException = Assertions.assertThrows(DomainException.class, () -> actualCategory.validate(new ThrowsValidationHandler()));
+        Assertions.assertEquals(expectedErrorCount,actualException.getErrors().size());
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+    }
+
+    @Test
+    public void givenAnInvalidNameLengthMoreThan255_whenCallNewCategoryAndValidate_thenShouldReceiveError() {
+        final String expectedName = """
+                   O cuidado em identificar pontos críticos na estrutura atual da organização facilita a criação das novas proposições. Nunca é demais lembrar o peso e o significado destes problemas, uma vez que o 
+                   desenvolvimento contínuo de distintas formas de atuação afeta positivamente a correta previsão do remanejamento dos quadros funcionais. Por conseguinte, o novo modelo estrutural aqui preconizado é uma das 
+                   consequências dos conhecimentos estratégicos para atingir a excelência. Assim mesmo, o acompanhamento das preferências de consumo nos obriga à análise dos índices pretendidos. A prática cotidiana prova que a 
+                   consolidação das estruturas obstaculiza a apreciação da importância dos níveis de motivação departamental     
+                """;
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'name' must be between 3 and 255 characters";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+
+        final var actualCategory = Category.newCategory(expectedName, expectedDescription, expectedIsActive);
+
+        final var actualException = Assertions.assertThrows(DomainException.class, () -> actualCategory.validate(new ThrowsValidationHandler()));
+        Assertions.assertEquals(expectedErrorCount,actualException.getErrors().size());
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+    }
+
+    @Test
+    public void givenAValidEmptyDescription_whenCallNewCategoryAndValidate_thenShouldReceiveError() {
+        final String expectedName = "Filmes";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+
+        final var actualCategory = Category.newCategory(expectedName, expectedDescription, expectedIsActive);
+
+        Assertions.assertNotNull(actualCategory);
+        Assertions.assertNotNull(actualCategory.getId());
+        Assertions.assertEquals(expectedName, actualCategory.getName());
+        Assertions.assertEquals(expectedDescription, actualCategory.getDescription());
+        Assertions.assertEquals(expectedIsActive, actualCategory.isActive());
+        Assertions.assertNotNull(actualCategory.getCreatedAt());
+        Assertions.assertNotNull(actualCategory.getUpdatedAt());
+        Assertions.assertNull(actualCategory.getDeletedAt());
+     }
+
+    @Test
+    public void givenAValidFalseIsActive_whenCallNewCategoryAndValidate_thenShouldReceiveError() {
+        final String expectedName = "Filmes";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = false;
+
+        final var actualCategory = Category.newCategory(expectedName, expectedDescription, expectedIsActive);
+
+        Assertions.assertNotNull(actualCategory);
+        Assertions.assertNotNull(actualCategory.getId());
+        Assertions.assertEquals(expectedName, actualCategory.getName());
+        Assertions.assertEquals(expectedDescription, actualCategory.getDescription());
+        Assertions.assertEquals(expectedIsActive, actualCategory.isActive());
+        Assertions.assertNotNull(actualCategory.getCreatedAt());
+        Assertions.assertNotNull(actualCategory.getUpdatedAt());
+        Assertions.assertNotNull(actualCategory.getDeletedAt());
     }
 }
